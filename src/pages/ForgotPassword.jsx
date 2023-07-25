129796f682fdd98d429d26363404e8703e6f3530
Ariel <arielsayag19@gmail.com>
2023-07-25 12:09:44 +0300

    complete the forgot password page  functionality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import OAuth from '../components/OAuth';
-
+import { toast } from 'react-toastify';
 import { InputForm ,LinkRed,LinkBlue, H1,ContainerDiv , ContainerImage , ContainerForm,ContainerTextLinks , SubmitFormButton , ORdiv } from '../styledSaas/SignCss';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { useNavigate } from 'react-router';
 
 const srcImg="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=773&q=80";
 
@@ -10,10 +12,23 @@ const srcImg="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b23
 function ForgotPassword() {
 
   const [email,setEmail]=useState("");
+  const nevigation=useNavigate();
 
   const handleChange=(e)=>{
     const mail=e.target.value;
     setEmail(mail);
+  }
+
+  const handleForgotPasswordSubmit=async(e)=>{
+    e.preventDefault();
+
+    try {
+      const auth=getAuth();
+      await sendPasswordResetEmail(auth,email);
+      toast.success("Email was sent.")
+    } catch (error) {
+      toast.error("Could not send reset password.")
+    }
   }
 
   return (
@@ -24,7 +39,7 @@ function ForgotPassword() {
             <img className=' w-full rounded-2xl ' src={srcImg} alt="key" ></img>
         </ContainerImage>
         <ContainerForm>
-          <form>
+          <form onSubmit={handleForgotPasswordSubmit}>
             <InputForm type='email' id="email" value={email} placeholder='Email address' onChange={handleChange}/>
             <ContainerTextLinks>
               <p>Don't have an account?
