a9695d57a4be0cd9570827cccf90d5e9492b233a
Ariel <arielsayag19@gmail.com>
2023-07-25 15:51:16 +0300

    add the Edit functionality to the profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,28 +1,54 @@
 import React, { useState } from 'react';
 import {H1,ContainerFormProfile,InputForm,ContainerTextLinks,SpanRed,LinkBlue,Section} from "../styledSaas/SignCss";
 import {FcHome} from "react-icons/fc";
-import { getAuth } from 'firebase/auth';
+import { getAuth, updateCurrentUser, updateProfile } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from '../firebase';
 
 export default function Profile() {
   const auth=getAuth();
   const nevigate=useNavigate();
-  const [formData,setFormData]=useState({name:"",email:""});
+  //get data from firebase
+  const [formData,setFormData]=useState({name:auth.currentUser.displayName,email:auth.currentUser.email});
   const {name,email}=formData;
+  const [changeDetails,setChangeDetails]=useState(false);
 
-
-
+  // chnage detail in the useState
   const handleChange=(e)=>{
     setFormData((current)=>({
       ...current,
       [e.target.id]:e.target.value
     }))
   };
+  //add the change to database
+  const onSubmit=async()=>{
+    try {
+      if(auth.currentUser.displayName !== name){
+        //update display name in firebase auth
+        await updateProfile(auth.currentUser,{
+          displayName:name,
+        })
+
+        //update name in firestore!
+        const docRef=doc(db,"users",auth.currentUser.uid)
+        await updateDoc(docRef,{
+          name,
+        })
+      }
+      toast.success("Profile details  updated.")
+    } catch (error) {
+      toast.error("Could not update the profile details")
+    }
+  }
+
 
   const handleSubmit=(e)=>{
     e.preventDefualt();
   }
 
+  //LogOut
   const handleSignOut=()=>{
     auth.signOut();
     nevigate("/");
@@ -32,13 +58,18 @@ export default function Profile() {
     <H1>My Profile</H1>
       <ContainerFormProfile>
         <form onSubmit={handleSubmit}>
-          <InputForm type='text' disabled id="name" value={name} placeholder='Full name' onChange={handleChange}/>
-          <InputForm type='email' disabled id="email" value={email} placeholder='Email address' onChange={handleChange}/>
+          <InputForm type='text' disabled={!changeDetails} id="name" value={name}  onChange={handleChange} className={changeDetails?'opacity-100':'opacity-60'} />
+          <InputForm type='email' disabled={!changeDetails} id="email" value={email}  onChange={handleChange} className={changeDetails?'opacity-100':'opacity-60'} />
           
     
           <ContainerTextLinks>
             <p>Do want to change your name?
-              <SpanRed >Edit</SpanRed>
+              <SpanRed onClick={()=>{
+                changeDetails && onSubmit(); 
+                setChangeDetails((current)=>!current)}
+              }>
+               {changeDetails ? "Apply Change" : "Edit"}
+              </SpanRed>
             </p>
             <p>
               <LinkBlue onClick={handleSignOut}>Sign Out</LinkBlue>
